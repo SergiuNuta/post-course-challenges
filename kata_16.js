15b926e911736eaaff45858df0bfcccf4d4adbb5
Serghei Nuta <Serghei.Nuta@Nology.local>
2020-05-13 21:44:42 +0100

    completed katas 15 and 16

--- kata_16.js
+++ kata_16.js
@@ -1,9 +1,6 @@
 // How do you check if a string contains only digits?
 
+const digitsOnlyCheck = (string) => parseInt(string).toString() === string ? `The string "${string}" contains only digits!` : `The string "${string}" do not contains only digits!`
 
-const digitsOnly = (string) => {
-    return parseInt(string);
-}
-
-console.log(digitsOnly('123'));
+console.log(digitsOnlyCheck('12345'));
 
