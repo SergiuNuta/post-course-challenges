39a320db68af0903d92882c1afb2a24b59509b63
Serghei Nuta <Serghei.Nuta@Nology.local>
2020-05-13 12:05:21 +0100

    first commit

---
+++ kata_7.js
@@ -0,0 +1,6 @@
+// How do you reverse the words in a sentence?
+
+const sentence = 'The man fell off a cliff';
+
+let reverseSentence = sentence.split(' ').reverse().join(' ');
+console.log(reverseSentence);
